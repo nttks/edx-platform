f4bd0e530450c2669086cd0180e6a2304b58e3cc
hachiyanagi-ks <m.hachiyanagi@nttks.co.jp>
2015-04-16 13:02:37 +0900

    Merge pull request #8 from yokose-ks/EDX-860
    EDX-860: Allow only admin user to handle personal information in instruc...
    (cherry picked from commit c801e96a6d929e77e688604f5d6639e82dfec491)
    
    Conflicts:
    	lms/envs/common.py

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -269,6 +269,12 @@ FEATURES = {
     # only edX superusers can perform the downloads)
     'ALLOW_COURSE_STAFF_GRADE_DOWNLOADS': False,
 
+    # Give course staff unrestricted access to profile downloads
+    'ALLOW_COURSE_STAFF_PROFILE_DOWNLOADS': False,
+
+    # Give course staff unrestricted access to view student gradebook
+    'ALLOW_COURSE_STAFF_VIEW_GRADEBOOK': False,
+
     'ENABLED_PAYMENT_REPORTS': [
         "refund_report",
         "itemized_purchase_report",
