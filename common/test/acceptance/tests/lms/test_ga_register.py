adcc815a59629d0d0c49057821da347e270f2cd1
Akihito Yokose <ayokose.gacco@gmail.com>
2015-05-11 18:59:06 +0900

    Merge pull request #52 from hachiyanagi-ks/develop/birch-10-marketing-link
    Add tests for marketing links #10
    (cherry picked from commit 17f0367e83d3a761991fea65a51d3971b5378a04)

--- common/test/acceptance/tests/lms/test_ga_register.py
+++ common/test/acceptance/tests/lms/test_ga_register.py
@@ -4,13 +4,16 @@ End-to-end tests for the LMS registration flow.
 """
 
 import re
+import requests
 from nose.plugins.attrib import attr
 
 from bok_choy.web_app_test import WebAppTest
 from lms.envs.bok_choy import EMAIL_FILE_PATH
 from ...fixtures.course import CourseFixture
+from ...pages.lms.ga_header_footer import HeaderPage, FooterPage
 from ...pages.lms.ga_register import RegisterPage, ActivationPage
 from ..ga_helpers import EmailTestMixin
+
 
 @attr('shard_ga')
 class RegistrationTest(WebAppTest, EmailTestMixin):
@@ -38,6 +41,8 @@ class RegistrationTest(WebAppTest, EmailTestMixin):
         super(RegistrationTest, self).setUp()
 
         self.register_page = RegisterPage(self.browser)
+        self.header_page = HeaderPage(self.browser)
+        self.footer_page = FooterPage(self.browser)
 
         # setup mail client
         self.setup_email_client(EMAIL_FILE_PATH)
@@ -51,6 +56,37 @@ class RegistrationTest(WebAppTest, EmailTestMixin):
             return matches.groupdict()['token']
         raise Exception('Activation key not found.')
 
+    def assert_header_footer_link(self, is_login):
+        ## Header links
+        # Check link of logo
+        self.assertEqual(
+            'http://gacco.org/',
+            requests.get(self.header_page.logo_link, allow_redirects=False).headers['Location']
+        )
+        if is_login:
+            # Check link of FAQ
+            self.assertEqual(
+                'https://support.gacco.org/',
+                requests.get(self.header_page.navigation_menu_links[0], allow_redirects=False).headers['Location']
+            )
+
+        ## Footer links
+        # Check link of terms of service
+        self.assertEqual(
+            'https://support.gacco.org/hc/ja/articles/200749224',
+            requests.get(self.footer_page.get_navigation_link('nav-colophon-01'), allow_redirects=False).headers['Location']
+        )
+        # Check link of privacy policy
+        self.assertEqual(
+            'https://support.gacco.org/hc/ja/articles/200749314',
+            requests.get(self.footer_page.get_navigation_link('nav-colophon-02'), allow_redirects=False).headers['Location']
+        )
+        # Check link of FAQ
+        self.assertEqual(
+            'https://support.gacco.org/',
+            requests.get(self.footer_page.get_navigation_link('nav-colophon-03'), allow_redirects=False).headers['Location']
+        )
+
     def test_register_and_activate(self):
         # Create a course to register for
         CourseFixture(
@@ -61,6 +97,9 @@ class RegistrationTest(WebAppTest, EmailTestMixin):
         # Visit register page
         self.register_page.visit()
 
+        # Check marketing link in header and footer
+        self.assert_header_footer_link(is_login=False)
+
         # User ragistration
         self.register_page.provide_info(self.EMAIL, self.PASSWORD, self.USERNAME, self.FULL_NAME)
         dashboard_page = self.register_page.submit()
@@ -68,6 +107,9 @@ class RegistrationTest(WebAppTest, EmailTestMixin):
         # Check successfully registerered and logged in.
         self.assertIn(self.EMAIL, dashboard_page.activation_message[0])
         self.assertIn("Looks like you haven't enrolled in any courses yet.", dashboard_page.current_courses_text)
+
+        # Check marketing link in header and footer
+        self.assert_header_footer_link(is_login=True)
 
         # Check activation email.
         activation_message = self.email_client.get_latest_message()
