c3386ae742f1872d0dabe7e5e59c12cee2fb59e3
hachiyanagi-ks <m.hachiyanagi@gacco.co.jp>
2015-10-07 13:34:28 +0900

    Add employee_number to registration #92

--- openedx/core/djangoapps/user_api/views.py
+++ openedx/core/djangoapps/user_api/views.py
@@ -24,6 +24,7 @@ from openedx.core.lib.api.permissions import ApiKeyHeaderPermission
 import third_party_auth
 from django_comment_common.models import Role
 from edxmako.shortcuts import marketing_link
+from microsite_configuration import microsite
 from student.views import create_account_with_params
 from student.cookies import set_logged_in_cookies
 from openedx.core.lib.api.authentication import SessionAuthenticationAllowInactiveUser
@@ -157,6 +158,7 @@ class RegistrationView(APIView):
     DEFAULT_FIELDS = ["email", "name", "username", "password"]
 
     EXTRA_FIELDS = [
+        "employee_number",
         "city",
         "country",
         "gender",
@@ -185,7 +187,9 @@ class RegistrationView(APIView):
 
         # Backwards compatibility: Honor code is required by default, unless
         # explicitly set to "optional" in Django settings.
-        self._extra_fields_setting = copy.deepcopy(settings.REGISTRATION_EXTRA_FIELDS)
+        self._extra_fields_setting = copy.deepcopy(
+            microsite.get_value('REGISTRATION_EXTRA_FIELDS', settings.REGISTRATION_EXTRA_FIELDS)
+        )
         self._extra_fields_setting["honor_code"] = self._extra_fields_setting.get("honor_code", "required")
 
         # Check that the setting is configured correctly
@@ -707,6 +711,14 @@ class RegistrationView(APIView):
             error_messages={
                 "required": error_msg
             }
+        )
+
+    def _add_employee_number_field(self, form_desc, required=True):
+        label = _(u"Employee Number")
+        form_desc.add_field(
+            "employee_number",
+            label=label,
+            required=required
         )
 
     def _apply_third_party_auth_overrides(self, request, form_desc):
