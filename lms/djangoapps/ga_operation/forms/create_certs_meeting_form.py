6199a4b2750b61927118ad1222fde99d81ea180d
Yoshitaka Sakamaki <y.sakamaki@gacco.co.jp>
2016-04-08 13:57:16 +0900

    Modify web on the result message of publish certificate from ga_operation tools. #617

--- lms/djangoapps/ga_operation/forms/create_certs_meeting_form.py
+++ lms/djangoapps/ga_operation/forms/create_certs_meeting_form.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
-from ga_operation.forms.ga_operation_base_form import GaOperationBaseForm
+from ga_operation.forms.ga_operation_base_form import GaOperationBaseForm, GaOperationEmailField
 
 
 class CreateCertsMeetingForm(GaOperationBaseForm):
     cert_pdf_tmpl = forms.FileField(required=True)
     cert_pdf_meeting_tmpl = forms.FileField(required=True)
     cert_lists = forms.FileField(required=True)
-    email = forms.CharField(required=True)
+    email = GaOperationEmailField(required=True)
 
--- lms/djangoapps/ga_operation/forms/ga_operation_base_form.py
+++ lms/djangoapps/ga_operation/forms/ga_operation_base_form.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import re
+
 from django import forms
+from django.conf import settings
 
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
@@ -15,3 +18,13 @@ class GaOperationBaseForm(forms.Form):
         except InvalidKeyError:
             raise forms.ValidationError(u"講座IDの書式が不正です。")
         return val
+
+
+class GaOperationEmailField(forms.EmailField):
+    def clean(self, value):
+        super(GaOperationEmailField, self).clean(value)
+        # permit valid domains only.
+        for domain in settings.GA_OPERATION_VALID_DOMAINS_LIST:
+            if re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@{}$'.format(domain), value):
+                return value
+        raise forms.ValidationError(u"このドメインのEメールは使用できません。")
