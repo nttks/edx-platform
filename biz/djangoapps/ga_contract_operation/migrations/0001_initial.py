6bd42499b8bebf2035940d4b7e1c70099b86af99
hachiyanagi-ks <m.hachiyanagi@gacco.co.jp>
2016-07-19 11:52:26 +0900

    Re-create migration files #852

--- biz/djangoapps/ga_contract_operation/migrations/0001_initial.py
+++ biz/djangoapps/ga_contract_operation/migrations/0001_initial.py
@@ -1,136 +1,43 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'ContractTaskHistory'
-        db.create_table('ga_contract_operation_contracttaskhistory', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('contract', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ga_contract.Contract'])),
-            ('task_id', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, db_index=True)),
-            ('requester', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal('ga_contract_operation', ['ContractTaskHistory'])
+    dependencies = [
+        ('ga_contract', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('ga_invitation', '0001_initial'),
+    ]
 
-        # Adding model 'ContractTaskTarget'
-        db.create_table('ga_contract_operation_contracttasktarget', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('history', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ga_contract_operation.ContractTaskHistory'])),
-            ('register', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ga_invitation.ContractRegister'])),
-            ('completed', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal('ga_contract_operation', ['ContractTaskTarget'])
-
-        # Adding unique constraint on 'ContractTaskTarget', fields ['history', 'register']
-        db.create_unique('ga_contract_operation_contracttasktarget', ['history_id', 'register_id'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'ContractTaskTarget', fields ['history', 'register']
-        db.delete_unique('ga_contract_operation_contracttasktarget', ['history_id', 'register_id'])
-
-        # Deleting model 'ContractTaskHistory'
-        db.delete_table('ga_contract_operation_contracttaskhistory')
-
-        # Deleting model 'ContractTaskTarget'
-        db.delete_table('ga_contract_operation_contracttasktarget')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'ga_contract.contract': {
-            'Meta': {'object_name': 'Contract'},
-            'contract_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'contract_type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'contractor_organization': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'org_contractor_contracts'", 'to': "orm['ga_organization.Organization']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
-            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'end_date': ('django.db.models.fields.DateField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'invitation_code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'owner_organization': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'org_owner_contracts'", 'to': "orm['ga_organization.Organization']"}),
-            'start_date': ('django.db.models.fields.DateField', [], {})
-        },
-        'ga_contract_operation.contracttaskhistory': {
-            'Meta': {'object_name': 'ContractTaskHistory'},
-            'contract': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ga_contract.Contract']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'requester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'db_index': 'True'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        },
-        'ga_contract_operation.contracttasktarget': {
-            'Meta': {'unique_together': "(('history', 'register'),)", 'object_name': 'ContractTaskTarget'},
-            'completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'history': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ga_contract_operation.ContractTaskHistory']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'register': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ga_invitation.ContractRegister']"})
-        },
-        'ga_invitation.contractregister': {
-            'Meta': {'unique_together': "(('contract', 'user'),)", 'object_name': 'ContractRegister'},
-            'contract': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'contract_register'", 'to': "orm['ga_contract.Contract']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'status': ('django.db.models.fields.CharField', [], {'default': "'Input'", 'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
-        },
-        'ga_organization.organization': {
-            'Meta': {'object_name': 'Organization'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
-            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'creator_org': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'creator_orgs'", 'to': "orm['ga_organization.Organization']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'org_code': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'org_name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['ga_contract_operation']
+    operations = [
+        migrations.CreateModel(
+            name='ContractTaskHistory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('task_id', models.CharField(max_length=255, null=True, db_index=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('contract', models.ForeignKey(to='ga_contract.Contract')),
+                ('requester', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ContractTaskTarget',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('completed', models.BooleanField(default=False)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('history', models.ForeignKey(to='ga_contract_operation.ContractTaskHistory')),
+                ('register', models.ForeignKey(to='ga_invitation.ContractRegister')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='contracttasktarget',
+            unique_together=set([('history', 'register')]),
+        ),
+    ]
--- openedx/core/djangoapps/ga_task/migrations/0001_initial.py
+++ openedx/core/djangoapps/ga_task/migrations/0001_initial.py
@@ -1,86 +1,31 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Task'
-        db.create_table('ga_task_task', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('task_type', self.gf('django.db.models.fields.CharField')(max_length=50, db_index=True)),
-            ('task_key', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
-            ('task_input', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('task_id', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
-            ('task_state', self.gf('django.db.models.fields.CharField')(max_length=50, db_index=True)),
-            ('task_output', self.gf('django.db.models.fields.CharField')(max_length=1024, null=True)),
-            ('requester', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('subtasks', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal('ga_task', ['Task'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Task'
-        db.delete_table('ga_task_task')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'ga_task.task': {
-            'Meta': {'object_name': 'Task'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'requester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'subtasks': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
-            'task_input': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'task_key': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
-            'task_output': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True'}),
-            'task_state': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'task_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['ga_task']
+    operations = [
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('task_type', models.CharField(max_length=50, db_index=True)),
+                ('task_key', models.CharField(max_length=255, db_index=True)),
+                ('task_input', models.CharField(max_length=255)),
+                ('task_id', models.CharField(max_length=255, db_index=True)),
+                ('task_state', models.CharField(max_length=50, db_index=True)),
+                ('task_output', models.CharField(max_length=1024, null=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('subtasks', models.TextField(blank=True)),
+                ('requester', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
