4f74e04094e846df35eea98910883e44303cf5cd
Akihito Yokose <ayokose.gacco@gmail.com>
2016-07-27 19:22:06 +0900

    Merge pull request #1041 from yokose-ks/develop/cypress/update-biz-playback-status-batch
    Add update biz playback status batch #1039
    (cherry picked from commit af250e42f0c260e0b7a32eb5f12816a61a3e82f0)

--- biz/djangoapps/ga_achievement/tests/factories.py
+++ biz/djangoapps/ga_achievement/tests/factories.py
@@ -2,7 +2,7 @@
 from factory.django import DjangoModelFactory
 
 from biz.djangoapps.ga_achievement.models import ScoreBatchStatus
-from biz.djangoapps.ga_achievement.achievement_store import ScoreStore
+from biz.djangoapps.ga_achievement.achievement_store import ScoreStore, PlaybackStore
 from biz.djangoapps.util.tests.factories import BizMongoFactory
 
 
@@ -12,6 +12,13 @@ class ScoreFactory(BizMongoFactory):
         model = ScoreStore
 
     init_args = ['contract_id', 'course_id']
+
+
+class PlaybackFactory(BizMongoFactory):
+
+    FACTORY_FOR = PlaybackStore
+
+    init_args = ['course_id', 'target_id']
 
 
 class ScoreBatchStatusFactory(DjangoModelFactory):
