514f7424530fd10d028b457e1bfaca3e854685d8
Yoshitaka Sakamaki <y.sakamaki@gacco.co.jp>
2016-08-19 19:03:29 +0900

    Revert "Remove links to profile page. #227"
    This reverts commit 82cb1f064daeefe5bfc28dbbd0b93717c85e1735.
    
    Conflicts:
    	common/test/acceptance/pages/lms/ga_discussion.py

--- common/test/acceptance/pages/lms/ga_discussion.py
+++ common/test/acceptance/pages/lms/ga_discussion.py
@@ -1,22 +1,7 @@
 from .discussion import (
-    DiscussionUserProfilePage as EdXDiscussionUserProfilePage,
     DiscussionTabHomePage as EdXDiscussionTabHomePage
 )
 
-
-class DiscussionUserProfilePage(EdXDiscussionUserProfilePage):
-
-    def is_browser_on_page(self):
-        return (
-            self.q(css='section.discussion-user-threads[data-course-id="{}"]'.format(self.course_id)).present
-            and
-            self.q(css='section.user-profile div.sidebar-username').present
-            and
-            self.q(css='section.user-profile div.sidebar-username').text[0] == self.username
-        )
-
-    def exists_sidebar_username_link_element(self):
-        return self.q(css='section.user-profile div.sidebar-username > a').present
 
 class DiscussionTabHomePage(EdXDiscussionTabHomePage):
 
