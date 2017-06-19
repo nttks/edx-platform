ff7f2c95cf465336c23afe6ed7d56f24304b765e
h.kawaguchi <kawaguchi-ks@users.noreply.github.com>
2017-06-19 13:56:16 +0900

    Add transaction control for mask-user command. #1950 (#1952)
    * Add transaction control for mask-user command. #1950
    
    * Fix review myself.

--- biz/djangoapps/ga_student/management/commands/mask_user.py
+++ biz/djangoapps/ga_student/management/commands/mask_user.py
@@ -7,6 +7,7 @@ import logging
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from biz.djangoapps.util import mask_utils, datetime_utils
 from openedx.core.djangoapps.course_global.models import CourseGlobalSetting
@@ -36,12 +37,24 @@ class Command(BaseCommand):
         )
         global_courses_ids = set(CourseGlobalSetting.all_course_id())
 
+        failed_user_ids = []
+
         for user in mask_users:
-            log.info(u"Masked user_id : {}.".format(user.id))
-            mask_utils.optout_receiving_global_course_emails(user, global_courses_ids)
-            mask_utils.mask_name(user)
-            mask_utils.mask_email(user)
-            mask_utils.disconnect_third_party_auth(user)
-            Registration.objects.get(user=user).update_masked()
+            log.info(u"Masked start user_id : {}.".format(user.id))
+            try:
+                with transaction.atomic():
+                    mask_utils.optout_receiving_global_course_emails(user, global_courses_ids)
+                    mask_utils.mask_name(user)
+                    mask_utils.mask_email(user)
+                    mask_utils.disconnect_third_party_auth(user)
+                    Registration.objects.get(user=user).update_masked()
+            except:
+                log.exception(u"Masked failed user_id : {}.".format(user.id))
+                failed_user_ids.append(user.id)
+            else:
+                log.info(u"Masked success user_id : {}.".format(user.id))
+
+        if failed_user_ids:
+            log.error(u"Masked failed user_ids : {}".format(failed_user_ids))
 
         log.info(u"Command mask_user finished at {}.".format(datetime_utils.timezone_now()))
